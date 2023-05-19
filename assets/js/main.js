e11647404376b5d6dbc760ccf1b488186f72a843
ShioriItai <siycha@gmail.com>
2023-05-20 01:29:49 +0900

    2023/05/20

--- assets/js/main.js
+++ assets/js/main.js
@@ -88,19 +88,21 @@ $(function(){
     });
 });
 
-/*検索部分をクリックした後の画面表示 */
+/*検索部分をクリックした後の画面表示*/
 $(function(){
-    $('.open-btn').click(function(){
-        $('.open-nav,.close-btn-x').addClass('active');
-        $('#open-btn-search').addClass('active');
+    $('.search_container').click(function(){
+        $('.bottom-menu').addClass('active');
+        $('.fa-search').addClass('active');
+        $('.bottom-close-x').addClass('active');
     });
 });
 
-/*検索部分をクリックした後の画面を閉じる */
+/*検索部分をクリックした後の画面を閉じる*/
 $(function(){
-    $('.close-btn-x').click(function(){
-        $('.open-nav').removeClass('active');
-        $('#open-btn-search').removeClass('active');
+    $('.bottom-close-x').click(function(){
+        $('.bottom-menu').removeClass('active');
+        $('.fa-search').removeClass('active');
+        $('.bottom-close-x').removeClass('active');
     });
 });
 
@@ -204,3 +206,42 @@ $(function(){
         $(this).removeClass('active');
     });
 })
+
+function delayScrollAnime() {
+	var time = 0.2;//遅延時間を増やす秒数の値
+	var value = time;
+	$('.delayScroll').each(function () {
+		var parent = this;					//親要素を取得
+		var elemPos = $(this).offset().top;//要素の位置まで来たら
+		var scroll = $(window).scrollTop();//スクロール値を取得
+		var windowHeight = $(window).height();//画面の高さを取得
+		var childs = $(this).children();	//子要素を取得
+		
+		if (scroll >= elemPos - windowHeight && !$(parent).hasClass("play")) {//指定領域内にスクロールが入ったらまた親要素にクラスplayがなければ
+			$(childs).each(function () {
+				
+				if (!$(this).hasClass("fadeUp")) {//アニメーションのクラス名が指定されているかどうかをチェック
+					
+					$(parent).addClass("play");	//親要素にクラス名playを追加
+					$(this).css("animation-delay", value + "s");//アニメーション遅延のCSS animation-delayを追加し
+					$(this).addClass("fadeUp");//アニメーションのクラス名を追加
+					value = value + time;//delay時間を増加させる
+					
+					//全ての処理を終わったらplayを外す
+					var index = $(childs).index(this);
+					if((childs.length-1) == index){
+						$(parent).removeClass("play");
+					}
+				}
+			})
+		}else {
+			$(childs).removeClass("fadeUp");//アニメーションのクラス名を削除
+			value = time;//delay初期値の数値に戻す
+		}
+	})
+}
+
+// 画面をスクロールをしたら動かしたい場合の記述
+	$(window).scroll(function (){
+		delayScrollAnime();/* アニメーション用の関数を呼ぶ*/
+	});// ここまで画面をスクロールをしたら動かしたい場合の記述
